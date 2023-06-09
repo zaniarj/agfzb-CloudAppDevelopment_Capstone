cb68269ba9851d460509cbdcc82f22c9ea833809
zaniarj <zanyar.j@gmail.com>
2023-06-09 19:49:19 -0400

    sign-up

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -52,8 +52,35 @@ def logout_request(request):
 
 
 # Functions for `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+   context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:get_dealerships")
+        else:
+            context={"msq":"Username already exists!"}
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
