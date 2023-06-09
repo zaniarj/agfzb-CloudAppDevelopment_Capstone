17cd43d9f8ebb62e73d24ddb9d2c7d583bd597fe
zaniarj <zanyar.j@gmail.com>
2023-06-09 19:32:24 -0400

    second

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,11 @@ urlpatterns = [
     # path for registration
 
     # path for login
-
+    path(route='login/',view=views.login_request,name='login'),
     # path for logout
+    path(route='logout/',view=views.logout_request,name='logout'),
 
+    #root
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,27 +14,44 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
 
 
-# Create an `about` view to render a static about page
+
+# Function for `about` view to render a static about page
 def about(request):
      return render(request,'djangoapp/about.html')
 
 
-# Create a `contact` view to return a static contact page
+# Functions for `contact` view to return a static contact page
 def contact(request):
      return render(request,'djangoapp/contact.html')
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+# Functions for `login_request` view to handle sign in request
+def login_request(request):
+    context = {}
+    if request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            messages.info(request, 'Your input is incorrect!')
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
-# Create a `registration_request` view to handle sign up request
+# Functions for `logout_request` view to handle sign out request
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'Logout success!')
+    return render(request, 'djangoapp/index.html')
+
+
+# Functions for `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
 
@@ -45,11 +62,11 @@ def get_dealerships(request):
         return render(request, 'djangoapp/index.html', context)
 
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
+# Functions for `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
 
-# Create a `add_review` view to submit a review
+# Functions for `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
 
